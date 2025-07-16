f0bcf5159e574f018337e6893acecde410685887
bycait27 <caitlinba27@gmail.com>
2025-07-16 14:46:18 -0500

    Fixes to tailwindcss config and removed unnecessary css from existing files.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        
+      },
+      animation: {
+        
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
